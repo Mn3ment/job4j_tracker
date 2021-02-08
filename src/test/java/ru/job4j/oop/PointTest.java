828ed7c055a6d5b46c6a30d80c65f9755e8e147b
Mn3ment <muviment@gmail.com>
2021-02-08 23:22:00 +0300

    PointTest

---
+++ src/test/java/ru/job4j/oop/PointTest.java
@@ -0,0 +1,29 @@
+package ru.job4j.oop;
+
+import org.junit.Test;
+import org.junit.Assert;
+
+import static org.junit.Assert.assertThat;
+
+public class PointTest {
+
+    @Test
+    public void distance() {
+        int expected = 4;
+        Point a = new Point(8, 4);
+        Point b = new Point(8, 8);
+        double out = a.distance(b);
+        Assert.assertEquals(expected, out, 0.01);
+
+    }
+
+    @Test
+    public void distance3d() {
+        double expected = 2.23606797749979;
+        Point a = new Point(0, 0, 3);
+        Point b = new Point(0, 2, 4);
+        double out = a.distance3d(b);
+        Assert.assertEquals(expected, out, 0.01);
+
+    }
+}
