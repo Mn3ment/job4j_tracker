f95c30466f7d1d54cce72dd32285b5bceef95937
Mn3ment <muviment@gmail.com>
2021-02-12 17:03:52 +0300

    5. Tracker - хранилище [#415856]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -1,4 +1,5 @@
 package ru.job4j.tracker;
+import java.util.Arrays;
 
 public class Tracker {
     private final Item[] items = new Item[100];
@@ -21,5 +22,31 @@ public class Tracker {
             }
         }
         return rsl;
+    }
+
+    public Item[] findAll() {
+        Item[] namesWithoutNull = new Item[items.length];
+        int size = 0;
+        for (int index = 0; index < items.length; index++) {
+            if  (items[index] != null) {
+                namesWithoutNull[size] = items[index];
+                size++;
+            }
+        }
+        namesWithoutNull = Arrays.copyOf(namesWithoutNull, size);
+        return namesWithoutNull;
+    }
+    public Item[] findByName(String key) {
+        Item[] findName = new Item[items.length];
+        int size = 0;
+        for (int index = 0; index < items.length; index++) {
+            Item item = items[index];
+            if (item.getName().equals(key)) {
+                findName[size] = item;
+                size++;
+            }
+        }
+        findName = Arrays.copyOf(findName, size);
+        return findName;
     }
 }
