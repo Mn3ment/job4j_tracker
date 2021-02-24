c44f1a14275b25dbe85df5c305cb15d5c670732c
Mn3ment <muviment@gmail.com>
2021-02-24 18:46:09 +0300

    5. Input и полиморфизм. [#415780]

--- src/main/java/ru/job4j/tracker/Item.java
+++ src/main/java/ru/job4j/tracker/Item.java
@@ -7,7 +7,7 @@ public class Item {
     private int id;
     private String name;
     private LocalDateTime created = LocalDateTime.now();
-
+    public Item() {}
     public Item(String name) {
         this.name = name;
     }
