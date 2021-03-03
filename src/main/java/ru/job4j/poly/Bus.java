a78e4d634145019d153c729862696f2f135c93c4
Mn3ment <muviment@gmail.com>
2021-03-03 16:44:10 +0300

    3. Интерфейс Input fix

--- src/main/java/ru/job4j/poly/Bus.java
+++ src/main/java/ru/job4j/poly/Bus.java
@@ -3,16 +3,22 @@ package ru.job4j.poly;
 public class Bus implements Transport {
     @Override
     public void drive() {
+        String pointA = "Москва";
+        String pointB = "Владивасток";
+        System.out.println("Рейс " + pointA + "- " + pointB + "отправляется через 5 минут");
 
     }
 
     @Override
     public void passengers(int pass) {
+        System.out.println("Общее количество пассажиров = " + pass);
 
     }
 
     @Override
     public int refuel(int fuel, int price) {
+        int fullTank = 40;
+        price = fuel * fullTank;
         return price;
     }
 }
