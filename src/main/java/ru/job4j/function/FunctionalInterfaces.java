cbdbe19b46d196665e70b1ee216f5507408a6fbe
Mn3ment <muviment@gmail.com>
2021-06-08 15:24:30 +0300

    1.1. Что такое import (Из 1го модуля)

---
+++ src/main/java/ru/job4j/function/FunctionalInterfaces.java
@@ -0,0 +1,13 @@
+package ru.job4j.function;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.function.*;
+
+public class FunctionalInterfaces {
+    public static void main(String[] args) {
+        Map<Integer, String> map = new HashMap<>();
+        }
+    }
