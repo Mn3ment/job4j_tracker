c5d3543909b780c593a3d369b1b0fb0e79f9d437
Mn3ment <muviment@gmail.com>
2021-06-27 00:12:55 +0300

    0.1. Отладка лямбда в IDEA [#259165]

--- src/main/java/ru/job4j/stream/Even.java
+++ src/main/java/ru/job4j/stream/Even.java
@@ -12,8 +12,8 @@ public class Even {
             numbers.add(i);
         }
         List<Integer> even = numbers.stream().filter(
-                number -> number % 2 == 0
-        ).collect(Collectors.toList());
-        even.forEach(System.out::println);
+                number -> number % 2 == 0)
+                .collect(Collectors.toList());
+                even.forEach(System.out::println);
     }
 }
