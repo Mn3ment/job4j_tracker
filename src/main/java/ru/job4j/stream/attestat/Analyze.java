cc72c4555fb11f503aa14c57877e4984a201fc65
Mn3ment <muviment@gmail.com>
2021-07-02 15:40:06 +0300

    2. Аттестация

---
+++ src/main/java/ru/job4j/stream/attestat/Analyze.java
@@ -0,0 +1,62 @@
+package ru.job4j.stream.attestat;
+
+import java.util.Comparator;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class Analyze {
+
+    public static double averageScore(Stream<Pupil> stream) {
+        return stream.flatMap(pupil -> pupil.getSubjects().stream())
+                .mapToInt(Subject::getScore)
+                .average()
+                .orElse(0);
+
+    }
+
+    public static List<Tuple> averageScoreBySubject(Stream<Pupil> stream) {
+        return stream.map(e -> new Tuple(e.getName(), e.getSubjects().stream()
+                .mapToInt(Subject::getScore)
+                .average()
+                .orElse(0)))
+                .collect(Collectors.toList());
+    }
+
+    public static List<Tuple> averageScoreByPupil(Stream<Pupil> stream) {
+        return stream
+                .flatMap(pupil -> pupil.getSubjects().stream())
+                .collect(Collectors.groupingBy(Subject::getName, LinkedHashMap::new,
+                        Collectors.averagingDouble(Subject::getScore)))
+                .entrySet()
+                .stream()
+                .map(pupil ->  new Tuple(
+                        pupil.getKey(),
+                        pupil.getValue()))
+                .collect(Collectors.toList());
+    }
+
+    public static Tuple bestStudent(Stream<Pupil> stream) {
+        return stream.map(e -> new Tuple(e.getName(), e.getSubjects().stream()
+                .mapToInt(Subject::getScore)
+                .sum()))
+                .max(Comparator.comparing(Tuple::getScore))
+                .orElse(null);
+    }
+
+    public static Tuple bestSubject(Stream<Pupil> stream) {
+        return stream
+                .flatMap(pupil -> pupil.getSubjects().stream())
+                .collect(Collectors.groupingBy(Subject::getName, LinkedHashMap::new,
+                        Collectors.summingDouble(Subject::getScore)))
+                .entrySet()
+                .stream()
+                .map(pupil ->  new Tuple(
+                        pupil.getKey(),
+                        pupil.getValue()))
+                .max(Comparator.comparing(Tuple::getScore))
+                .orElse(null);
+
+    }
+}
